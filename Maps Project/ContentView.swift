62b4b6a3b1c3cf1232b1652ecca7d73ce3dc5dce
James Fitch <fitchy@me.com>
2019-11-11 22:13:24 +1100

    Initial Commit

---
+++ Maps Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maps Project
+//
+//  Created by James Fitch on 11/11/19.
+//  Copyright © 2019 Fitchatron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
